af6edea53568ad8e9a052cdc4798a81a07019742
Jake Melville <jakemelville@ymail.com>
2021-03-03 18:24:39 -0500

    made all my functions asyncronous. Created an addTeamMember function and added switch statement inside to deal with the user asking to add an intern/ engineer.

--- app.js
+++ app.js
@@ -8,161 +8,199 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-
-
+const render = require("./lib/htmlRenderer.js");
 
 const members = [];
 const idArr = [];
 
-// function to create a manager 
-function menu() {
-    function createManager() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'managerName',
-                message: 'What is the managers name?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'managerId',
-                message: 'What is the managers id?',
-                validate: answer => {
-                    if (answer !== "") {
+async function addTeamMember() {
+    const memberChoice = await inquirer.prompt({
+        type: 'list',
+        name: 'addMember',
+        message: 'Which type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', 'None']
 
-                        //validate that the id's do not match need to ADD this in to make sure ids, and passwords dont match
-                        return true;
-                    }
-                    return "Please enter at least once character";
+    })
+    switch (memberChoice.addMember) {
+        case 'Engineer':
+            const engineerInfo = await createEngineer();
+            const engineer = new Engineer(engineerInfo.engineerName, engineerInfo.engineerId, engineerInfo.engineerEmail, engineerInfo.engineerGit);
+            members.push(engineer);
+            idArr.push(engineer.id);
+            addTeamMember();
+            break;
+        case 'Intern':
+            const internInfo = await createIntern();
+            const intern = new Intern(internInfo.internName, internInfo.internId, internInfo.internEmail, internInfo.internSchool);
+            members.push(intern);
+            idArr.push(intern.id);
+            addTeamMember();
+
+        default: 
+        console.log('default');
+
+
+    };
+}
+
+async function createManager() {
+    return await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            message: 'What is the managers name?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
                 }
-            },
-            {
-                type: 'input',
-                name: 'managerEmail',
-                message: 'What is the managers email address?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'officeNum',
-                message: 'What is the managers office number?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
+                return "Please enter at least once character";
             }
-        ])
-    }
-    function createEngineer() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'engineerName',
-                message: 'What is your name?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'What is the managers id?',
+            validate: answer => {
+                if (answer !== "") {
+
+                    //validate that the id's do not match need to ADD this in to make sure ids, and passwords dont match
+                    return true;
                 }
-            },
-            {
-                type: 'input',
-                name: 'engineerId',
-                message: 'What is the engineers id?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'engineerEmail',
-                message: 'What is the engineers email?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'engineerGit',
-                messgae: 'What is the engineers github username?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
+                return "Please enter at least once character";
             }
-        ])
-    }
-    function createIntern() {
-        inquirer.prompt([
-            {
-                type: 'input',
-                name: 'internName',
-                message: 'What is your name?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is the managers email address?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
                 }
-            },
-            {
-                type: 'input',
-                name: 'internId',
-                message: 'What is the interns id?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'internEmail',
-                message: 'What is the interns email?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
-            },
-            {
-                type: 'input',
-                name: 'internSchool',
-                messgae: 'What school did the intern attend?',
-                validate: answer => {
-                    if (answer !== "") {
-                        return true;
-                    }
-                    return "Please enter at least once character";
-                }
+                return "Please enter at least once character";
             }
-        ])
-    }
+        },
+        {
+            type: 'input',
+            name: 'officeNum',
+            message: 'What is the managers office number?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        }
+    ])
+}
+async function createEngineer() {
+    return await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is your name?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: 'What is the engineers id?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the engineers email?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGit',
+            messgae: 'What is the engineers github username?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        }
+    ])
+}
+async function createIntern() {
+    return await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is your name?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: 'What is the interns id?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the interns email?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            messgae: 'What school did the intern attend?',
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least once character";
+            }
+        }
+    ])
+}
+
+
+async function menu() {
+    const managerInfo = await createManager();
+    const manager = new Manager(managerInfo.managerName, managerInfo.managerId, managerInfo.managerEmail, managerInfo.officeNum);
+
+    members.push(manager);
+    idArr.push(manager.id);
+
+    const memberSelect = await addTeamMember();
+
+   
 
 }
 menu();
